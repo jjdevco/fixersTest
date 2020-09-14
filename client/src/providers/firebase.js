3861b1732ead364774ad8a2d049887edbe9c39a7
Jose Jimenez <58991130+jjdevco@users.noreply.github.com>
2020-09-13 20:04:59 -0500

    initialize firebase

---
+++ client/src/providers/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const CLIENT_CONFIG = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_CLIENT_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+firebase.initializeApp(CLIENT_CONFIG);
+
+export const auth = firebase.auth;
