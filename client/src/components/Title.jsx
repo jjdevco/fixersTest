ea7f1fa6eb7d86f53e28bf0af27b4d30be87c827
Jose Jimenez <58991130+jjdevco@users.noreply.github.com>
2020-09-15 18:34:39 -0500

    create representational components

---
+++ client/src/components/Title.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  width: 100%;
+  margin: ${(props) => props.theme.spacing(10, 0)};
+  font-size: 1.7rem;
+`;
+
+export default Title;
