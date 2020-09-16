77a58c0f52e69646bf9419041ac02f4b1ecf3653
Jose Jimenez <58991130+jjdevco@users.noreply.github.com>
2020-09-16 01:15:55 -0500

    build upload files middleware

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,10 +1,15 @@
+const env = require("dotenv").config();
 const functions = require("firebase-functions");
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const uploadHandler = require("./middlewares/uploadHandler");
 const errorHandler = require("./middlewares/errorHandler");
+
 const app = express();
+
+const clientsRoutes = require("./routes/clients");
 
 // Middlewares Initialization
 app.use(
@@ -14,12 +19,13 @@ app.use(
     allowedHeaders: "Origin,X-Requested-With,Content-Type,Authorization,Accept",
   })
 );
-app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(uploadHandler);
 
-app.get("/test", hasName, (req, res, next) => {
-  res.send({ message: "hello World" });
-});
+// Inject Routes
+app.use(clientsRoutes);
 
 // Error Handler Middleware
 app.use(errorHandler);
--- server/functions/middlewares/authenticationHandler.js
+++ server/functions/middlewares/authenticationHandler.js
@@ -1,8 +1,8 @@
 const { admin } = require("../providers/firebase");
 
 module.exports = async (req, res, next) => {
+  const authorization = req.headers.authorization;
   try {
-    const authorization = req.headers.authorization;
     if (authorization && authorization.includes("Bearer ")) {
       const idToken = authorization.split("Bearer ")[1];
       const user = await admin.auth().verifyIdToken(idToken);
@@ -14,6 +14,11 @@ module.exports = async (req, res, next) => {
       throw error;
     }
   } catch (error) {
+    console.info({
+      error: "Wrong credentials",
+      authorization,
+      date: new Date().getTime(),
+    });
     next(error);
   }
 };
